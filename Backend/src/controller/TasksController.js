4f01d2446d5fa1578ba2721ad0ace7995c4d3e8d
shubham <shubhamsagar628@gmail.com>
2024-10-30 16:35:23 +0530

    update tasks, status and fetch it

--- Backend/src/controller/TasksController.js
+++ Backend/src/controller/TasksController.js
@@ -61,7 +61,167 @@ const getAllTasks = async(req,res) => {
     }
 }
 
+const deleteTasks = async(req,res) => {
+    try {
+        
+        const {id} = req.params
+        const user = req.user 
+
+        const deleteTasks = await Tasks.findByIdAndDelete(id) 
+        const updateUser = await User.findByIdAndUpdate(user._id,{$pull:{tasks:id}}) 
+
+        return res.status(200).json({
+            success:true,
+            message:'Tasks Deleted Successfully'
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+ 
+
+const updateTaks = async(req,res) => {
+    try {
+        
+        const {id} = req.params 
+        const {title,description} = req.body
+
+        const updateTasks = await Tasks.findByIdAndUpdate(id,{title,description})
+
+        return res.status(200).json({
+            success:true,
+            message:"Tasks Update Successfully"
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+const importantTasks = async(req,res) => {
+    try {
+        const {id} = req.params 
+
+        const importantTasksStatus = await Tasks.findById(id)
+
+      if(!importantTasksStatus){
+        return res.status(400).json({
+            success:false,
+            message:'Tasks Not Found'
+        })
+      }
+
+      importantTasksStatus.important = !importantTasksStatus.important 
+      await importantTasksStatus.save()
+
+        return res.status(200).json({
+            success:true,
+            message:'Important Tasks Added Successfully'
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:message.error
+        })
+    }
+}
+
+const completeTasks = async (req,res) => {
+    try {
+        
+        const {id} = req.params 
+
+
+        const tasks = await Tasks.findById(id) 
+
+        if(!tasks){
+            return res.status(400).json({
+                success:false,
+                message:'Tasks Not Found'
+            })
+        }
+
+        tasks.completed = !tasks.completed 
+        await tasks.save()
+
+        return res.status(200).json({
+            success:true,
+            message:'Tasks Update Status of complete'
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+const getImportantTasks = async(req,res) => {
+    try {
+        
+        const user = req.user 
+
+        const findImportantTasks = await User.findById(user._id).select('-password').populate({
+            path:"tasks",
+            options:{sort:{createdAt:-1}},
+            match:{important:true}
+        })
+
+        return res.status(200).json({
+            success:true,
+            message:'All important Tasks fetch Successfully',
+            data:findImportantTasks
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+
+const getCompleteTasks = async(req,res) => {
+    try {
+        const user = req.user
+
+        const findCompletedTasks = await User.findById(user._id).select('-password').populate({
+            path:'tasks',
+            options:{sort:{createdAt:-1}},
+            match:{completed:true}
+        })
+
+        return res.status(200).json({
+            success:true,
+            message:"Get Complete Tasks fetch successfully",
+            data:findCompletedTasks
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
 module.exports = {
     createTaks,
-    getAllTasks
+    getAllTasks,
+    deleteTasks,
+    updateTaks,
+    importantTasks,
+    completeTasks,
+    getImportantTasks,
+    getCompleteTasks
 }
--- Backend/src/routes/TasksRoutes.js
+++ Backend/src/routes/TasksRoutes.js
@@ -1,10 +1,16 @@
  const express = require('express')
 const { authUser } = require('../middleware/auth')
-const { createTaks, getAllTasks } = require('../controller/TasksController')
+const { createTaks, getAllTasks, deleteTasks, updateTaks, importantTasks, completeTasks, getImportantTasks, getCompleteTasks } = require('../controller/TasksController')
 
 const TasksRouter = express.Router() 
 
 TasksRouter.post("/createTasks",authUser,createTaks) 
 TasksRouter.get("/getAllTasks",authUser,getAllTasks)
+TasksRouter.delete("/deleteTasks/:id",authUser,deleteTasks)
+TasksRouter.put("/updateTasks/:id",authUser,updateTaks)
+TasksRouter.get("/importantTasks/:id",authUser,importantTasks)
+TasksRouter.get("/completeTasks/:id",authUser,completeTasks)
+TasksRouter.get("/importantTasks",authUser,getImportantTasks)
+TasksRouter.get("/completeTasks",authUser,getCompleteTasks)
 
 module.exports = TasksRouter
