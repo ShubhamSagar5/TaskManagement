3196947edd85a888d2838264886307f4f899bdda
shubham <shubhamsagar628@gmail.com>
2024-11-01 11:25:33 +0530

    Backend Done

--- Backend/src/middleware/auth.js
+++ Backend/src/middleware/auth.js
@@ -4,7 +4,7 @@ const { User } = require('../models/UserModel')
 const authUser = async(req,res,next) => {
     try {
         
-        const {token} = req.cookies
+        const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(" ")[1])
 
         if(!token){
             return res.status(404).json({
